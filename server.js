62646dc85b5f0a50489451118474e1453eadea72
Alex Cooter <56034009+alex-lc@users.noreply.github.com>
2020-02-12 16:31:27 -0600

    almost done with MVP

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const express = require('express');
+const helmet = require('helmet');
+
+const postsRouter = require('./posts/postRouter');
+const userRouter = require('./users/userRouter');
 
 const server = express();
 
-server.get('/', (req, res) => {
+// middleware
+server.use(express.json());
+server.use(helmet());
+
+server.use('/api/posts', logger, postsRouter);
+server.use('/api/users', logger, userRouter);
+
+server.get('/', logger, (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  const date = Date(Date.now());
+  console.log(`${req.method} Request to ${req.originalUrl} on ${date}`);
+  next();
+}
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,39 +1,82 @@
 const express = require('express');
 
+const Users = require('./userDb');
+
 const router = express.Router();
 
-router.post('/', (req, res) => {
+// middleware
+router.use(express.json());
+
+router.post('/', logger, (req, res) => {
+
+});
+
+router.post('/:id/posts', logger, (req, res) => {
   // do your magic!
 });
 
-router.post('/:id/posts', (req, res) => {
+// GET list of all users
+router.get('/', logger, (req, res) => {
+  Users.get()
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: "There was an error retrieving the list of users." });
+    });
+});
+
+// GET by user id
+router.get('/:id', logger, validateUserId, (req, res) => {
+  Users.getById(req.params.id)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => {
+      res.status(400).json({ error: "Invalid user id." });
+    })
+});
+
+// GET posts by id
+router.get('/:id/posts', logger, validateUserId, (req, res) => {
+  Users.getUserPosts(req.params.id)
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      res.status(400).json({ error: "Invalid user id." });
+    })
+});
+
+router.delete('/:id', logger, (req, res) => {
   // do your magic!
 });
 
-router.get('/', (req, res) => {
-  // do your magic!
-});
-
-router.get('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
-});
-
-router.delete('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.put('/:id', (req, res) => {
+router.put('/:id', logger, (req, res) => {
   // do your magic!
 });
 
 //custom middleware
+function logger(req, res, next) {
+  const date = Date(Date.now());
+  console.log(`${req.method} Request to ${req.originalUrl} on ${date}`);
+  next();
+}
 
 function validateUserId(req, res, next) {
-  // do your magic!
+  Users.getById(req.params.id)
+    .then(user => {
+      if (user) {
+        next();
+      }
+      else {
+        res.status(400).json({ error: "Invalid user id." });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ error: "Something went wrong." });
+    })
 }
 
 function validateUser(req, res, next) {
